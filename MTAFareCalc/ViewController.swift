d424ac8438252152d494040d3e32c808b81659d5
Brian Balayon <brianbalayon@gmail.com>
2020-06-27 17:01:38 -0400

    Initialized the Xcode project using UI Storyboards

---
+++ MTAFareCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTAFareCalc
+//
+//  Created by Brian Anthony M. Balayon on 6/27/20.
+//  Copyright © 2020 Brian Anthony M. Balayon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
